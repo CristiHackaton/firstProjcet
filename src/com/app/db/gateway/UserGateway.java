a08dc3f87f92d53c8f3f14d227dfe41c49219d94
dannyell <daniel.fratean@evozon.com>
2014-04-01 22:00:49 +0300

    fix project

--- src/com/app/db/gateway/UserGateway.java
+++ src/com/app/db/gateway/UserGateway.java
@@ -7,27 +7,26 @@ import java.sql.SQLException;
 
 import com.app.db.ConnectionWithDB;
 import com.app.db.model.User;
-import com.app.exception.DatabaseException;
 
 public class UserGateway {
 
     // CRUD Statements
     private static String CREATE_STATEMENT = "insert into user values (?,?,?,?,?)";
     private static String READ_BY_ID_STATEMENT = "select * from user where id = ?";
-    private static String READ_BY_ID_USER = "select * from user where user_name = ?";
+    private static String READ_BY_ID_USER_NAME = "select * from user where user_name = ?";
     private static String UPDATE_BY_COLUMN_STATEMENT = "update user set user_name = ?, password = ?, email = ? where ?=? ";
     private static String REMOVE_STATEMENT = "delete from user where id = ?";
 
-    public boolean addUser(final User userToAdd) throws DatabaseException {
+    public boolean addUser(final User userToAdd) {
         Connection con = ConnectionWithDB.getInstance();
 
         try {
             PreparedStatement addStatement = con.prepareStatement(CREATE_STATEMENT);
-            addStatement.setInt(0, userToAdd.getID());
-            addStatement.setString(1, userToAdd.getUserName());
+            addStatement.setInt(0, userToAdd.getUserID());
+            addStatement.setString(1, userToAdd.getUsername());
             addStatement.setString(2, userToAdd.getPassword());
             addStatement.setString(3, userToAdd.getEmail());
-            addStatement.setInt(4, userToAdd.getType());
+            addStatement.setInt(4, userToAdd.getUserType());
 
             addStatement.executeUpdate();
 
@@ -49,18 +48,45 @@ public class UserGateway {
 
             if (result.first()) {
                 User user = new User();
-                user.setId(result.getInt("idUser"));
-                user.setUserName(result.getString("user_name"));
+                user.setUserID(result.getInt("idUser"));
+                user.setUsername(result.getString("user_name"));
                 user.setPassword(result.getString("password"));
                 user.setEmail(result.getString("email"));
-                user.setType(result.getInt("type"));
+                user.setUserType(result.getInt("type"));
 
                 return user;
             } else {
                 return null;
             }
         } catch (SQLException e) {
-            System.out.println("User cannot be added!");
+            System.out.println("User cannot be found with id " + id);
+            return null;
+        }
+
+    }
+
+    public User getUserById(final String username) {
+        Connection con = ConnectionWithDB.getInstance();
+
+        try {
+            PreparedStatement getStatement = con.prepareStatement(READ_BY_ID_STATEMENT);
+            getStatement.setString(0, username);
+            ResultSet result = getStatement.executeQuery();
+
+            if (result.first()) {
+                User user = new User();
+                user.setUserID(result.getInt("idUser"));
+                user.setUsername(result.getString("user_name"));
+                user.setPassword(result.getString("password"));
+                user.setEmail(result.getString("email"));
+                user.setUserType(result.getInt("type"));
+
+                return user;
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            System.out.println("User cannot be found with usernam " + username);
             return null;
         }
 
